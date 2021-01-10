ebbf1cace3e4cf41340eb04ba253af99c6546880
Carl <hey@heying.site>
2021-01-10 14:00:45 +0800

    add CaseInsensitiveDictObj

--- heyy/core.py
+++ heyy/core.py
@@ -50,7 +50,7 @@ def reflect(obj, *, skip_callable=False, exclude: Optional[AttrNames] = None):
 
 class DictObj:
 
-    __invalid_attrs = frozenset(filter(lambda x: not x.startswith('__'), dir(dict)))
+    __reserved_attrs = frozenset(filter(lambda x: not x.startswith('__'), dir(dict)))
 
     def __init__(self, data: Optional[dict] = None):
         if data is None:
@@ -81,31 +81,72 @@ class DictObj:
     def __iter__(self):
         return iter(self.__dict__)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str):
         try:
             return getattr(self.__dict__, name)
         except AttributeError:
             pass
         return self.__getattribute__(name)
 
-    def __setattr__(self, name, value):
-        if name in self.__invalid_attrs:
+    def __setattr__(self, name: str, value):
+        if name in self.__reserved_attrs:
             raise ValueError(f'Invalid attr name <{name}>')
         return super().__setattr__(name, value)
 
 
-def json2obj(data: Any):
+def json2obj(data: Any, *, ignore_case=False):
+    dict_obj_class = DictObj if not ignore_case else CaseInsensitiveDictObj
     if isinstance(data, dict):
         data = data.copy()
         for k, v in data.items():
-            data[k] = json2obj(v)
-        return DictObj(data)
+            data[k] = json2obj(v, ignore_case=ignore_case)
+        return dict_obj_class(data)
     elif isinstance(data, list):
         data = data.copy()
         for i, v in enumerate(data):
-            data[i] = json2obj(v)
+            data[i] = json2obj(v, ignore_case=ignore_case)
         return data
     elif isinstance(data, tuple):
-        return json2obj(list(data))
+        return json2obj(list(data), ignore_case=ignore_case)
     else:
         return data
+
+
+class CaseInsensitiveDictObj(DictObj):
+
+    def __init__(self, data: Optional[dict] = None):
+        if isinstance(data, dict):
+            data = {
+                (k.lower() if isinstance(k, str) else k): v
+                for k, v in data.items()
+            }
+        super().__init__(data)
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            key = key.lower()
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
+            key = key.lower()
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        if isinstance(key, str):
+            key = key.lower()
+        super().__delitem__(key)
+
+    def __contains__(self, item):
+        if isinstance(item, str):
+            item = item.lower()
+        return super().__contains__(item)
+
+    def __getattr__(self, name: str):
+        return super().__getattr__(name.lower())
+
+    def __setattr__(self, name: str, value):
+        super().__setattr__(name.lower(), value)
+
+    def __delattr__(self, name: str) -> None:
+        super().__delattr__(name.lower())
