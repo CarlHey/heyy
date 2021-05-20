e6e0b5c976172fa2ed76d6ff4204fc10fceca445
Carl <hey@heying.site>
2021-05-20 20:20:54 +0800

    add new implementation of DictObj & CaseInsensitiveDictObj

--- heyy/core.py
+++ heyy/core.py
@@ -1,11 +1,23 @@
 from functools import wraps
 from os import chdir, getcwd
 from pathlib import Path
-from typing import Any, Optional, Iterable
+from typing import Any, Optional, Iterable, MutableMapping, TypeVar, Mapping
 
 from .pathtool import PathTool
 
 pathtool = PathTool()
+
+_T = TypeVar("_T")
+_S = TypeVar("_S")
+_KT = TypeVar("_KT")  # Key type.
+_VT = TypeVar("_VT")  # Value type.
+_T_co = TypeVar("_T_co", covariant=True)  # Any type covariant containers.
+_V_co = TypeVar("_V_co", covariant=True)  # Any type covariant containers.
+_KT_co = TypeVar("_KT_co", covariant=True)  # Key type covariant containers.
+_VT_co = TypeVar("_VT_co", covariant=True)  # Value type covariant containers.
+_T_contra = TypeVar("_T_contra", contravariant=True)  # Ditto contravariant.
+
+_null = object()
 
 
 def with_folder(folder, create_if_not_exists=True):
@@ -94,6 +106,133 @@ class DictObj:
         return super().__setattr__(name, value)
 
 
+# 2021年5月19日12:40±
+class DictObj2(dict, MutableMapping[_KT, _VT]):
+
+    def __init__(self, *args, **kw) -> None:
+        super().__init__(*args, **kw)
+
+    @classmethod
+    def fromkeys(cls, iterable: Iterable[_T], value: _S = _null) -> 'DictObj2[_T, _S]':
+        if value is _null:
+            result = super().fromkeys(iterable)
+        else:
+            result = super().fromkeys(iterable, value)
+        return cls(result)
+
+    def __setattr__(self, name: str, value):
+        self[name] = value
+
+    def __getattr__(self, name: str) -> _VT:
+        try:
+            return super().__getattribute__(name)
+        except AttributeError:
+            if name in self:
+                return self[name]
+            raise
+
+    def __delattr__(self, name: str):
+        try:
+            return super().__delattr__(name)
+        except AttributeError:
+            if name in self:
+                del self[name]
+                return
+            raise
+
+    def copy(self) -> 'DictObj2[_KT, _VT]':
+        return self.__class__(super().copy())
+
+
+def _lower_str_iterable_wrap(iterable: Iterable[_T]) -> Iterable[_T]:
+    for i in iterable:
+        if isinstance(i, str):
+            i = i.lower()
+        yield i
+
+
+def _lowered_if_str(o: _T) -> _T:
+    if isinstance(o, str):
+        o = o.lower()
+    return o
+
+
+# 2021年5月19日13:08:18
+class CaseInsensitiveDictObj2(DictObj2[_KT, _VT]):
+
+    def __init__(self, *args, **kw) -> None:
+        super().__init__()
+        self.update(*args, **kw)
+
+    def update(self, *args, **kwargs) -> None:
+        if len(args) > 1:
+            raise TypeError('update expected at most 1 arguments, got %d' % len(args))
+        if args:
+            other = args[0]
+            if isinstance(other, Mapping):
+                for key in other:
+                    self[_lowered_if_str(key)] = other[key]
+            elif hasattr(other, "keys"):
+                for key in other.keys():
+                    self[_lowered_if_str(key)] = other[key]
+            else:
+                for key, value in other:
+                    self[_lowered_if_str(key)] = value
+        for key, value in kwargs.items():
+            self[key.lower()] = value
+
+    @classmethod
+    def fromkeys(cls, iterable: Iterable[_T], value: _S = _null) -> 'CaseInsensitiveDictObj2[_T, _S]':
+        if value is _null:
+            result = super().fromkeys(_lower_str_iterable_wrap(iterable))
+        else:
+            result = super().fromkeys(_lower_str_iterable_wrap(iterable), value)
+        return cls(result)
+
+    def setdefault(self, key, default=None) -> _VT:
+        key = _lowered_if_str(key)
+        return super().setdefault(key, default)
+
+    def pop(self, key, default=_null):
+        key = _lowered_if_str(key)
+        if default is _null:
+            return super().pop(key)
+        else:
+            return super().pop(key, default)
+
+    def get(self, key, default=_null):
+        key = _lowered_if_str(key)
+        if default is _null:
+            return super().get(key)
+        else:
+            return super().get(key, default)
+
+    def __getitem__(self, k: _KT) -> _VT:
+        k = _lowered_if_str(k)
+        return super().__getitem__(k)
+
+    def __setitem__(self, k: _KT, v: _VT) -> None:
+        k = _lowered_if_str(k)
+        super().__setitem__(k, v)
+
+    def __delitem__(self, k: _KT) -> None:
+        k = _lowered_if_str(k)
+        super().__delitem__(k)
+
+    def __contains__(self, o: object) -> bool:
+        o = _lowered_if_str(o)
+        return super().__contains__(o)
+
+    def __setattr__(self, name: str, value):
+        super().__setattr__(name.lower(), value)
+
+    def __getattr__(self, name: str):
+        return super().__getattr__(name.lower())
+
+    def __delattr__(self, name: str):
+        super().__delattr__(name.lower())
+
+
 def json2obj(data: Any, *, ignore_case=False):
     dict_obj_class = DictObj if not ignore_case else CaseInsensitiveDictObj
     if isinstance(data, dict):
@@ -150,3 +289,12 @@ class CaseInsensitiveDictObj(DictObj):
 
     def __delattr__(self, name: str) -> None:
         super().__delattr__(name.lower())
+
+
+# 2021年3月25日01:14:44
+def select(obj: DictObj, attrs: Iterable[str]):
+    return obj.__class__({k: v for k, v in obj.items() if k in attrs})
+
+
+def omit(obj: DictObj, attrs: Iterable[str]):
+    return obj.__class__({k: v for k, v in obj.items() if k not in attrs})
