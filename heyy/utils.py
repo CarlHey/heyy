ac83b3b35e88b1e5c3c74727e2f54f4b2be41184
Carl <hey@heying.site>
2021-05-26 12:51:14 +0800

    Null type now use singleten pattern

--- heyy/utils.py
+++ heyy/utils.py
@@ -5,8 +5,16 @@ from .types import _T
 
 wraps_doc = partial(_wraps, assigned=('__doc__',))
 
+_singleton_null = None
+
 
 class Null:
+
+    def __new__(cls, *args, **kwargs):
+        global _singleton_null
+        if _singleton_null is None:
+            _singleton_null = super().__new__(cls, *args, **kwargs)
+        return _singleton_null
 
     def __repr__(self):
         return 'Null'
